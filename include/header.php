b6864e0390913a8764915aceabc2bada99a9e7c0
Thijs <thijsvandiessen@hotmail.com>
2016-04-19 18:41:19 +0200

    1 days work
    needs to be converted in php

---
+++ include/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Portfolio - Work of Thijs van Diessen - UX Designer</title>
+
+		<meta name="description" content="I am Thijs van Diessen, an UX Designer based in the Netherlands. Just graduated, specialised in prototyping and designing concepts and ideas.">
+		<meta name="keywords" content="Portfolio, Profile, Experience, Design, prototyping">
+		<meta name="author" content="Thijs van Diessen">
+
+		<link rel="icon" href="../../favicon.ico">
+
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
+		
+		<link href="my.css" rel="stylesheet">
+</head>
