04fdc3dfc08806948fb97459d246244b14ebb584
Thijs <thijsvandiessen@gmail.com>
2017-02-25 18:22:06 +0100

    start of new homepage

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php require('include/header.php');?>
 
-
+<div class="row full-images cover">
+	<div class="col-lg-12">
+		<h1>Hi, I'm a Front end developer</br> who likes to design too</h1>
+	</div>
+</div>
 
 <?php require('include/footer.php');?>
