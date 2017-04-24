74455495f6d0f1b26d6a8378d49aaa69f39d1f68
Thijs <thijsvandiessen@gmail.com>
2017-04-24 21:06:08 +0200

    cleaned js

--- js/all.js
+++ js/all.js
@@ -1,19 +1,18 @@
 $(function () {
 
+    // check if on homepage
     if ( window.location.pathname == 'index.php' ){
         // Index (home) page
-        console.log('index.php');
     } 
 
+    // local check if on homepage
     else if (window.location.pathname == '/~thijsvandiessen/portfolio/') {
         // Other page
-        console.log(window.location.pathname);
-
         
     }
 
     else {
-       // other 
+       // on other page
     }
 
 });
